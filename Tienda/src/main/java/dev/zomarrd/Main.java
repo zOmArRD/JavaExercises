fb33a689f10ffeaf054e5629c98415e29beeb712
zOmArRD <omarsena25@gmail.com>
2023-07-24 10:26:08 -0400

    Added Tienda module

---
+++ Tienda/src/main/java/dev/zomarrd/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Created by IntelliJ Idea Ultimate
+ *
+ * User: zOmArRD
+ * Date: 24/7/2023
+ *
+ * Copyright (c) 2023. OMY TECHNOLOGY <omar@omy.technology>
+ */
+
+package dev.zomarrd;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
