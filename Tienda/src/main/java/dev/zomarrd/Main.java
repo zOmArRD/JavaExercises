a1432f09358cd46f8610f82b88b6f7e291c57360
zOmArRD <omarsena25@gmail.com>
2023-07-24 14:10:08 -0400

    Added Tienda module

--- Tienda/src/main/java/dev/zomarrd/Main.java
+++ Tienda/src/main/java/dev/zomarrd/Main.java
@@ -9,7 +9,29 @@
 
 package dev.zomarrd;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double[] ventas = new double[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa las 10 ventas del mes: ");
+        for (int i = 0; i < ventas.length; i++) {
+            System.out.println("Venta " + (i + 1));
+            ventas[i] = scanner.nextDouble();
+        }
+
+        int k, total;
+        k = total = 0;
+
+        while (k < 10 ) {
+            if (ventas[k] >= 2000) {
+                System.out.println("$" + ventas[k]);
+                total++;
+            }
+            k++;
+        }
+        System.out.println("El total de ventas mayores a 2000 es: " + total);
     }
 }
